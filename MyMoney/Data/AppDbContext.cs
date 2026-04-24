77332079718531b3c75d092b187b7a010c9a02f4
Felipe <cc25130@g.unicamp.br>
2026-04-24 12:01:09 -0300

    Finalizando appDbContext

--- MyMoney/Data/AppDbContext.cs
+++ MyMoney/Data/AppDbContext.cs
@@ -8,11 +8,17 @@ namespace MyMoney.Data
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         public DbSet<Usuario> Usuario { get; set; }
+        public DbSet<Categoria> Categoria { get; set; }
+        public DbSet<Transacoes> Transacoes { get; set; }
+        public DbSet<Meta> Meta { get; set; }
+        public DbSet<Mensagem> Mensagem { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            
+            modelBuilder.HasDefaultSchema("MyMoney"); // Define o schema das tabelas
+
             // Configuração da tabela Usuario
-            modelBuilder.HasDefaultSchema("API");
             modelBuilder.Entity<Usuario>().HasKey(c => c.idUsuario);
             modelBuilder.Entity<Usuario>().Property(c => c.nome).IsRequired().HasColumnType("varchar(50)");
             modelBuilder.Entity<Usuario>().Property(c => c.email).IsRequired().HasColumnType("varchar(50)");
@@ -20,6 +26,36 @@ namespace MyMoney.Data
             modelBuilder.Entity<Usuario>().Property(c => c.moedaPadrao).IsRequired().HasColumnType("char(3)");
             modelBuilder.Entity<Usuario>().Property(c => c.dataCriacao).IsRequired().HasColumnType("datetime");
             modelBuilder.Entity<Usuario>().Property(c => c.idioma).IsRequired().HasColumnType("varchar(15)");
+
+            // Configuração da tabela Categoria
+            modelBuilder.Entity<Categoria>().HasKey(c => c.idCategoria);
+            modelBuilder.Entity<Categoria>().Property(c => c.NomeCategria).IsRequired().HasColumnType("varchar(30)");
+            modelBuilder.Entity<Categoria>().Property(c => c.Tipo).IsRequired().HasColumnType("varchar(10)");
+            modelBuilder.Entity<Categoria>().HasOne<Usuario>().HasForeignKey(c => c.idUsuario).IsRequired(false);
+
+            // Configuração da tabela Transações
+            modelBuilder.Entity<Transacoes>().HasKey(c => c.idTransacoes);
+            modelBuilder.Entity<Transacoes>().HasOne<Categoria>().HasForeignKey(c => c.idCategoria).IsRequired(false);
+            modelBuilder.Entity<Transacoes>().HasOne<Usuario>().HasForeignKey(c => c.idUsuario).IsRequired(true);
+            modelBuilder.Entity<Transacoes>().Property(c => c.Tipo).IsRequired().HasColumnType("varchar(10)");
+            modelBuilder.Entity<Transacoes>().Property(c => c.Valor).IsRequired().HasColumnType("money");
+            modelBuilder.Entity<Transacoes>().Property(c => c.Descricao).IsRequired().HasColumnType("varchar(30)");
+            modelBuilder.Entity<Transacoes>().Property(c => c.DataTransacao).IsRequired().HasColumnType("datetime");
+        
+            // Configuração da tabela Meta
+            modelBuilder.Entity<Meta>().HasKey(c => c.idMeta);
+            modelBuilder.Entity<Meta>().HasOne<Usuario>().HasForeignKey(c => c.idUsuario).IsRequired(true);
+            modelBuilder.Entity<Meta>().Property(c => c.NomeMeta).IsRequired().HasColumnType("varchar(20)");
+            modelBuilder.Entity<Meta>().Property(c => c.ValorObjetivo).IsRequired().HasColumnType("money");
+            modelBuilder.Entity<Meta>().Property(c => c.ValorAtual).IsRequired().HasColumnType("money");
+            modelBuilder.Entity<Meta>().Property(c => c.DataCriacao).IsRequired().HasColumnType("datetime");
+
+            // Configuração da tabela Mensagem
+            modelBuilder.Entity<Mensagem>().HasKey(c => c.idMensagem);
+            modelBuilder.Entity<Mensagem>().HasOne<Usuario>().HasForeignKey(c => c.idUsuario).IsRequired(true);
+            modelBuilder.Entity<Mensagem>().Property(c => c.Assunto).IsRequired().HasColumnType("varchar(15)");
+            modelBuilder.Entity<Mensagem>().Property(c => c.Mensagem).IsRequired().HasColumnType("varchar(50)");
+            modelBuilder.Entity<Mensagem>().Property(c => c.DataEnvio).IsRequired().HasColumnType("datetime"); 
         }
     }
 }
