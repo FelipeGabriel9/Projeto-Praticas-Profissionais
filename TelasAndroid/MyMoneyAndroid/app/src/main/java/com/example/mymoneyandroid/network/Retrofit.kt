0e4a27a0fda66d3d4a8ca7c9753043c771871c01
FELIPE GABRIEL TORRES DE JESUS <u25130@academico.cotuca.unicamp.br>
2026-05-11 08:52:26 -0300

    .

--- TelasAndroid/MyMoneyAndroid/app/src/main/java/com/example/mymoneyandroid/network/Retrofit.kt
+++ TelasAndroid/MyMoneyAndroid/app/src/main/java/com/example/mymoneyandroid/network/Retrofit.kt
@@ -3,10 +3,8 @@ package com.example.mymoneyandroid.network
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-// Tem que ser "object" e não "class"
-object RetrofitClient {
-    private const val urlBase = "http://10.0.2.2:5194/"
-
+object Retrofit {
+    private const val urlBase = "http://10.0.2.2:5194"
     val apiCategoria: CategoriaApi by lazy {
         Retrofit.Builder()
             .baseUrl(urlBase)
@@ -15,11 +13,12 @@ object RetrofitClient {
             .create(CategoriaApi::class.java)
     }
 
-    val apiUsuario: UsuarioApi by lazy {
+    val apiUsuario: CategoriaApi by lazy {
         Retrofit.Builder()
             .baseUrl(urlBase)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
-            .create(UsuarioApi::class.java)
+            .create(CategoriaApi::class.java)
     }
+
 }
--- TelasAndroid/MyMoneyAndroid/app/src/main/java/com/example/mymoneyandroid/network/UsuarioApi.kt
+++ TelasAndroid/MyMoneyAndroid/app/src/main/java/com/example/mymoneyandroid/network/UsuarioApi.kt
@@ -5,18 +5,13 @@ import com.example.mymoneyandroid.model.RegistroRequest
 import com.example.mymoneyandroid.model.UsuarioResponse
 import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface UsuarioApi {
+    @POST("usuarios/")
+    suspend fun cadastrarUsuario(@Body requisicao: RegistroRequest): Response<UsuarioResponse>
 
-    @POST("usuarios") // Esse é o caminho da sua rota de cadastro
-    suspend fun cadastrarUsuario(@Body request: RegistroRequest): Response<UsuarioResponse>
-
-    @POST("usuarios/login") // Esse é o caminho da sua rota de login
+    @POST("usuarios/login")
     suspend fun fazerLogin(@Body login: LoginRequest): Response<UsuarioResponse>
 
 }
-
-//@GET("usuarios/")
-//suspend fun perfilUsuario() : List<>
